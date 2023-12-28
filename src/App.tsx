d366bf154c1cebaf5f0d33ae5d53e2d8f687e811
Viktor Buzanov <buzanov.v@promail366.com>
2023-12-28 19:58:13 +0200

    fix pathname

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { NestGraphQLModel } from "./pages/nest-graphql-model/NestGraphQLModel";
 
 function App() {
   return (
-    <BrowserRouter basename="/multiconverter-json-ts">
+    <BrowserRouter>
       <Routes>
         <Route index element={<Home />}></Route>
         <Route path="graphql-nest" element={<NestGraphQLModel />} />
